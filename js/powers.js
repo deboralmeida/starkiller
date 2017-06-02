77a219e51ca23dca172b327d98f172c978a646c4
Débora Almeida <d.eboralmeida@hotmail.com>
2017-06-02 22:58:53 +0100

    fix: download, powerups responsive

--- js/powers.js
+++ js/powers.js
@@ -58,6 +58,7 @@ for (var i = 0; i < user.length; i++) {
             spread_left.classList.add('bullet');
             // add class .spread_left
             spread_left.classList.add('spread_left');
+            
             // RIGHT
             // add class .bullet
             spread_right.classList.add('bullet');
@@ -93,35 +94,18 @@ for (var i = 0; i < user.length; i++) {
             this.appendChild(friend);
             
             // create bullets
-                    friend_shot = document.createElement("div");
-                    // add class .bullet
-                    friend_shot.classList.add('bullet');
-                    // add class .friend_shot / change FRIEND's bullet color
-                    friend_shot.classList.add('friend_shot')
-                    // append
-                    friend.appendChild(friend_shot);
+            friend_shot = document.createElement("div");
+            // add class .bullet
+            friend_shot.classList.add('bullet');
+            // add class .friend_shot / change FRIEND's bullet color
+            friend_shot.classList.add('friend_shot')
+            // append
+            friend.appendChild(friend_shot);
                    
-            
-            // FRIEND'S BULLETS
-            /*idk = setInterval( function(){
-             
-                    //
-
-
-                }, 340);*/
-            
-            // FRIEND'S SHOOTING ANIMATION
-            /*cenas = setInterval( function(){
-                
-                    // add class .shoot
-                    friend_shot.classList.add('machine_gun');
- 
-                }, 350);*/
-            
             
         } else if (userID == 'user5'){
                 
-            // BARrIER
+            // BARRIER
             description.innerHTML = '<p class="title">Barrier</p>';
             
             // add class .barrier_on
@@ -143,40 +127,27 @@ for (var i = 0; i < user.length; i++) {
         
         userID = this.id;
         
-        // add class .shoot
+        // SHOOT
         if(userID == 'user3'){
+            
+            // add class .machine_gun
             bullet.classList.add('machine_gun');
+            
         } else {
+            
+            // add class .shoot
             bullet.classList.add('shoot');
+            
         }
         
         if (userID == 'user1'){
             
+            // add class .shoot_left
             spread_left.classList.add('shoot_left');
+            // add class .shoot_right
             spread_right.classList.add('shoot_right');
             
         }
-        
-        /*if (userID == 'user3'){
-            
-           idk = setInterval( function(){
-             
-                    machine_gun = document.createElement("div");
-
-                    machine_gun.classList.add('bullet');
-
-                    self.appendChild(machine_gun);
-                
-                }, 240);
-            
-            
-           cenas = setInterval( function(){
-                
-                    machine_gun.classList.add('shoot');
- 
-                }, 250);
-            
-        }*/
         
         // after animation time (500ms)
         clearBullet = setTimeout( function() { 
@@ -187,37 +158,37 @@ for (var i = 0; i < user.length; i++) {
                 bullet.classList.remove('shoot');
             
                 if (userID == 'user1'){
-                
+                    
+                    // remove spread_left
                     self.removeChild(spread_left);
+                    // remove class .shoot left
                     spread_left.classList.remove('shoot_left');
 
+                    // remove spread_right
                     self.removeChild(spread_right);
+                    // remove class .shoot_right
                     spread_right.classList.remove('shoot_right');
                     
                 }
-            
-                /*if (userID == 'user3'){
-                            
-                    self.removeChild(machine_gun);
-                    machine_gun.classList.remove('bullet');
-                    machine_gun.classList.remove('shoot');
-                            
-                }*/
         
             }, 500);
         
         // after animation time (500ms)
         spawnBullet = setTimeout( function() {
             
-                        // append child bullet
-                        self.appendChild(bullet);
+                // append child bullet
+                self.appendChild(bullet);
             
-                        if (userID == 'user1'){
-                            self.appendChild(spread_left);
-                            self.appendChild(spread_right);
-                        }
+                if (userID == 'user1'){
+                    
+                    // append spread_left
+                    self.appendChild(spread_left);
+                    // append spread_right
+                    self.appendChild(spread_right);
+                    
+                }
             
-                    }, 500);
+            }, 500);
     };
 
       
@@ -225,46 +196,56 @@ for (var i = 0; i < user.length; i++) {
     user[i].onmouseout = function() {
         
         userID = this.id;
-        
-        /*clearInterval(idk);
-        clearInterval(cenas);*/
+    
+        // clear setTimeOut
         clearTimeout(spawnBullet);
         clearTimeout(clearBullet);
         
         if (userID == 'user1'){
             
+            // remove spread_left
             this.removeChild(spread_left);
+            // remove spread_right
             this.removeChild(spread_right);
+            // remove class .shoot_left
             spread_left.classList.remove('shoot_left');
+            // remove class .shoot_right
             spread_right.classList.remove('shoot_right');
             
         } else if (userID == 'user2'){
             
+            // remove class .missile_shot
             bullet.classList.remove('missile_shot');
             
         } else if (userID == 'user3'){
             
-            //this.removeChild(machine_gun);
-            
+            // remove class .machine_gun
             bullet.classList.remove('machine_gun');
-            //machine_gun.classList.remove('bullet');
             
         } else if (userID == 'user4'){
             
+            // remove friend
             this.removeChild(friend);
+            // remove class .friend_anim
             friend.classList.remove('friend_anim');
             
+            // remove friend's bullets
             this.removeChild(friend_shot);
+            // remove class .bullet
             friend_shot.classList.remove('bullet');
             
         } else if (userID == 'user5'){
             
+            // change backgroundSize
             this.style.backgroundSize = 'contain';
+            // remove class .barrier_on
             barrier_on.classList.remove('barrier_on');
             
         }
         
-        this.removeChild(bullet);  
+        // remove bullets
+        this.removeChild(bullet);
+        // remove class .shoot
         bullet.classList.remove('shoot');
                 
     };
